35fd35a789013779973c12ee38ea46ed511dac7c
zhaokk <994979136kk@gmail.com>
2015-05-18 18:02:53 +1000

    new

---
+++ mvc 321/Models/course.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvc_321.Models
+{
+    public class course
+    {
+        public int courseId { get; set; }
+        public string CourseName { get; set; }
+        public int TotalCredits { get;set; }
+    }
+}
